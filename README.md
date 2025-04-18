eb514a4b94767232fd02daf21198c8b174bd5407
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2025-04-18 08:54:40 +0200

    Transiciones - Fin

--- README.md
+++ README.md
@@ -22,3 +22,29 @@ Creamos el proyecto, vamos a `main.dart` y lo vamos modificando.
 En la carpeta `lib` creamos la `pages` y dentro los archivos `pagina1_page.dart` y `pagina2_page.dart`.
 
 En `main.dart` llamamos a `pagina1_page.dart`.
+
+### SlideTransition
+
+Creamos la transición en la página en la cual queremos mandar llamar a la siguiente.
+
+Modificamos `pagina1_page.dart`.
+
+### ScaleTransition
+
+Modificamos `pagina1_page.dart`.
+
+### RotateTransition
+
+Modificamos `pagina1_page.dart`.
+
+### FadeTransition
+
+Si tenemos alguna página que usa un Loader, una vez tenemos la data podemos utilizar esta animación para dar el efecto de que estamos reemplazando los datos en pantalla, y el usuario no se da cuenta de que hemos navegado a otra pantalla distinta.
+
+Modificamos `pagina1_page.dart`.
+
+### Mezcla de transiciones
+
+Las transiciones se pueden mezclar, ejecutándose de manera simultanea.
+
+Modificamos `pagina1_page.dart`.
--- lib/pages/pagina1_page.dart
+++ lib/pages/pagina1_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:transicion_app/pages/pagina2_page.dart';
 
 class Pagina1Page extends StatelessWidget {
   const Pagina1Page({super.key});
@@ -15,8 +16,53 @@ class Pagina1Page extends StatelessWidget {
 
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.access_time),
-          onPressed: () {}
+          onPressed: () {
+            Navigator.push(context, _crearRuta());
+          }
         ),
+      );
+    }
+    
+    Route _crearRuta() {
+      return PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) => Pagina2Page(),
+        // transitionDuration: Duration(seconds: 2),
+
+        // Como queremos que esa nueva ruta sea construida.
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          
+          final curvedAnimation = CurvedAnimation(parent: animation, curve: Curves.easeInOut);
+
+          // return SlideTransition(
+          //   position: Tween<Offset>(begin: Offset(0.5, 1.0), end: Offset.zero).animate(curvedAnimation),
+          //   child: child,
+          // );
+
+          // return ScaleTransition(
+          //   scale: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
+          //   child: child,
+          // );
+
+          // return RotationTransition(
+          //   turns: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
+          //   child: child
+          // );
+
+          return FadeTransition(
+            opacity: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
+            child: child,
+          );
+
+        // Mezclando transiciones. Ambos se ejecutan de manera simultanea.
+        // return RotationTransition(
+        //   turns: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
+        //   child: FadeTransition(
+        //     opacity: Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation),
+        //     child: child,
+        //   ),
+        // );
+
+        },
       );
     }
 }
